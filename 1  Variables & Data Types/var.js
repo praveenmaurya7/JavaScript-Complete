29688e4c08254589c9996242f960028d85dd0d82
praveenmaurya7 <pkm4700@gmail.com>
2024-03-14 12:20:48 +0530

    Variables

---
+++ 1  Variables & Data Types/var.js
@@ -0,0 +1,30 @@
+// Variables are containers for Data.
+// strings are always stored in double or single quote.
+fullName = "Tony Strak";
+// for print the value of variable as it is, don't use double qoute in console.log
+console.log (fullName);
+
+age = 24;
+console.log (age);
+
+price = 99.99;
+console.log(price);
+
+x = null;
+// known but empty value
+console.log (x);
+y = undefined; 
+// undefined means don't know what it is
+console.log (y);
+
+// boolean type -> true value or false value
+
+isFollow = true;
+console.log (isFollow);
+
+// JS is a dynamically typed language because it can store any value in any variable.
+// don't tell about data type priorily
+// equal to (=) is assignment operator which assign the value
+
+fullName = 25;
+console.log(fullName)
